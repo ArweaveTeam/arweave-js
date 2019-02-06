e26805568e13aef0ee74861d44104368140f9d4e
Maximilien Mellen <maxmellen0@gmail.com>
2019-02-06 11:06:09 +0100

    Align node index with web index

--- src/node/index.ts
+++ src/node/index.ts
@@ -1,7 +1,8 @@
 import { Arweave } from "./arweave/arweave";
+import { ApiConfig } from "./arweave/lib/api";
 import { NodeCryptoDriver } from "./arweave/lib/crypto/node-driver";
 
-export function init(apiConfig: object): Arweave {
+export function init(apiConfig: ApiConfig = {}): Arweave {
   return new Arweave({
     api: apiConfig,
     crypto: new NodeCryptoDriver()
