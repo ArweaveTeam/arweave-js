c0484ee28cf4a852992bd625ee0fa69b90e58289
Kyle Beckles <kyle@arweave.org>
2019-10-23 16:23:46 -0400

    Version bump 1.5.1, updated init defaults to use arweave.net in dev envs

--- src/web/index.ts
+++ src/web/index.ts
@@ -9,19 +9,42 @@ declare global {
 }
 
 Arweave.init = function(apiConfig: ApiConfig = {}): Arweave {
-  function protocolToDefaultPort(protocol: string) {
-    if (protocol == "http") {
-      return 80;
+
+  function getDefaultConfig(): {
+    protocol: string;
+    host: string;
+    port: number;
+  } {
+    // window.location.protocol has a trailing colon (http:, https:, file: etc)
+    const currentProtocol = window.location.protocol.replace(":", "");
+    const currentHost = window.location.hostname;
+
+    const isLocal =
+      ["localhost", "127.0.0.1"].includes(currentHost) ||
+      currentProtocol == "file";
+
+    // If we're running in what looks like a local dev environment
+    // then default to using arweave.net
+    if (isLocal) {
+      return {
+        host: "arweave.net",
+        port: 443,
+        protocol: "https"
+      };
     }
-    if (protocol == "https") {
-      return 443;
-    }
+
+    return {
+      host: currentHost,
+      port: currentProtocol == "https" ? 443 : 80,
+      protocol: currentProtocol
+    };
   }
 
-  const protocol =
-    apiConfig.protocol || window.location.protocol.replace(":", "");
-  const host = apiConfig.host || window.location.hostname;
-  const port = apiConfig.port || protocolToDefaultPort(protocol);
+  const defaultConfig  = getDefaultConfig();
+
+  const protocol = apiConfig.protocol || defaultConfig.protocol;
+  const host = apiConfig.host || defaultConfig.host;
+  const port = apiConfig.port || defaultConfig.port;
 
   return new Arweave({
     api: {
