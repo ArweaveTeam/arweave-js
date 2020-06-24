38e0cc077dfa43e50928c106a936895c17f179e8
Kyle Beckles <kyle@arweave.org>
2020-06-24 02:06:49 +0200

    Added utility methods for chunking, full merkle trees,  proof generation

--- src/common/lib/merkle.ts
+++ src/common/lib/merkle.ts
@@ -1,80 +1,216 @@
-import Arweave from "../common";
-
-interface TaggedChunk {
-  id: Uint8Array;
-  end: number;
-}
-
 /**
- * Partial representation of an Arweave merkle tree node containing only the
- * information necessary for generating the root hash
+ * @see {@link https://github.com/ArweaveTeam/arweave/blob/fbc381e0e36efffa45d13f2faa6199d3766edaa2/apps/arweave/src/ar_merkle.erl}
  */
-interface HashNode {
-  readonly id: Uint8Array;
-  readonly max: number;
+import Arweave from "../common";
+import { concatBuffers } from "./utils";
+import { inspect } from "util";
+
+interface Chunk {
+  dataHash: Uint8Array;
+  minByteRange: number;
+  maxByteRange: number;
 }
+
+interface BranchNode {
+  readonly id: Uint8Array;
+  readonly type: "branch";
+  readonly byteRange: number;
+  readonly maxByteRange: number;
+  readonly leftChild?: MerkelNode;
+  readonly rightChild?: MerkelNode;
+}
+
+interface LeafNode {
+  readonly id: Uint8Array;
+  readonly dataHash: Uint8Array;
+  readonly type: "leaf";
+
+  readonly minByteRange: number;
+  readonly maxByteRange: number;
+}
+
+type MerkelNode = BranchNode | LeafNode;
 
 const CHUNK_SIZE = 256 * 1024;
 const NOTE_SIZE = 32;
 const HASH_SIZE = 32;
 
-export async function computeRootHash(data: Uint8Array): Promise<Uint8Array> {
-  let taggedChunks: TaggedChunk[] = [];
+/**
+ * Takes the input data and chunks it into (mostly) equal sized chunks.
+ * The last chunk will be a bit smaller as it contains the remainder
+ * from the chunking process.
+ */
+async function chunkData(data: Uint8Array): Promise<Chunk[]> {
+  let chunks: Chunk[] = [];
 
-  {
-    let rest = data;
-    let pos = 0;
+  let rest = data;
+  let cursor = 0;
 
-    while (rest.byteLength >= CHUNK_SIZE) {
-      let chunk = rest.slice(0, CHUNK_SIZE);
-      let id = await Arweave.crypto.hash(chunk);
-      pos += chunk.byteLength;
-      taggedChunks.push({ id, end: pos });
-      rest = rest.slice(CHUNK_SIZE);
-    }
-
-    taggedChunks.push({
-      id: await Arweave.crypto.hash(rest),
-      end: pos + rest.byteLength
+  while (rest.byteLength >= CHUNK_SIZE) {
+    const chunk = rest.slice(0, CHUNK_SIZE);
+    const dataHash = await Arweave.crypto.hash(chunk);
+    cursor += chunk.byteLength;
+    chunks.push({
+      dataHash,
+      minByteRange: cursor - chunk.byteLength,
+      maxByteRange: cursor,
     });
+    rest = rest.slice(CHUNK_SIZE);
   }
 
-  let nodes: HashNode[] = await Promise.all(
-    taggedChunks.map(({ id, end }) => hashLeaf(id, end))
-  );
+  chunks.push({
+    dataHash: await Arweave.crypto.hash(rest),
+    minByteRange: cursor,
+    maxByteRange: cursor + rest.byteLength,
+  });
 
-  while (nodes.length > 1) {
-    let nextNodes: HashNode[] = [];
-    for (let i = 0; i < nodes.length; i += 2) {
-      nextNodes.push(await hashBranch(nodes[i], nodes[i + 1]));
-    }
-    nodes = nextNodes;
-  }
-
-  const [{ id: rootHash }] = nodes;
-  return rootHash;
+  return chunks;
 }
 
-async function hashBranch(left: HashNode, right?: HashNode): Promise<HashNode> {
-  if (!right) {
-    return left;
+async function generateLeaves(chunks: Chunk[]): Promise<LeafNode[]> {
+  return Promise.all(
+    chunks.map(
+      async ({ dataHash, minByteRange, maxByteRange }): Promise<LeafNode> => {
+        return {
+          type: "leaf",
+          id: await hash(
+            await Promise.all([hash(dataHash), hash(intToBuffer(maxByteRange))])
+          ),
+          dataHash: dataHash,
+          minByteRange,
+          maxByteRange,
+        };
+      }
+    )
+  );
+}
+
+/**
+ * Builds an arweave merkle tree and gets the root hash for the given input.
+ */
+export async function computeRootHash(data: Uint8Array): Promise<Uint8Array> {
+  const rootNode = await generateTree(data);
+
+  return rootNode.id;
+}
+
+export async function generateTree(data: Uint8Array): Promise<MerkelNode> {
+  const rootNode = await buildLayers(
+    await generateLeaves(await chunkData(data))
+  );
+
+  return rootNode;
+}
+
+/**
+ * Starting with the bottom layer of leaf nodes, hash every second pair
+ * into a new branch node, push those branch nodes onto a new layer,
+ * and then recurse, building up the tree to it's root, where the
+ * layer only consists of two items.
+ */
+async function buildLayers(
+  nodes: MerkelNode[],
+  level = 0
+): Promise<MerkelNode> {
+  // If there are only 2 nodes left, this is going to be the root node
+  if (nodes.length < 2) {
+    const root = await hashBranch(nodes[0], nodes[1]);
+
+    // console.log("Root layer", root);
+
+    return root;
   }
 
-  return {
+  const nextLayer: MerkelNode[] = [];
+
+  for (let i = 0; i < nodes.length; i += 2) {
+    nextLayer.push(await hashBranch(nodes[i], nodes[i + 1]));
+  }
+
+  // console.log("Layer", nextLayer);
+
+  return buildLayers(nextLayer, level + 1);
+}
+
+/**
+ * Recursively search through all branches of the tree,
+ * and generate a proof for each leaf node.
+ */
+export function generateProofs(root: MerkelNode) {
+  const proofs = resolveBranchProofs(root);
+  return arrayFlatten<Proof>(proofs);
+}
+
+interface Proof {
+  offset: number;
+  proof: Uint8Array;
+}
+
+function resolveBranchProofs(
+  node: MerkelNode,
+  proof: Uint8Array = new Uint8Array(),
+  depth = 0
+): any {
+  if (node.type == "leaf") {
+    return {
+      offset: node.maxByteRange - 1,
+      proof: concatBuffers([
+        proof,
+        node.dataHash,
+        intToBuffer(node.maxByteRange),
+      ]),
+    };
+  }
+
+  if (node.type == "branch") {
+    const partialProof = concatBuffers([
+      proof,
+      node.leftChild!.id!,
+      node.rightChild!.id!,
+      intToBuffer(node.byteRange),
+    ]);
+    return [
+      resolveBranchProofs(node.leftChild!, partialProof, depth + 1),
+      resolveBranchProofs(node.rightChild!, partialProof, depth + 1),
+    ] as [Proof, Proof];
+  }
+}
+
+export function arrayFlatten<T = any>(input: T[]): T[] {
+  const flat: any[] = [];
+
+  input.forEach((item) => {
+    if (Array.isArray(item)) {
+      flat.push(...arrayFlatten(item));
+    } else {
+      flat.push(item);
+    }
+  });
+
+  return flat;
+}
+
+async function hashBranch(
+  left: MerkelNode,
+  right: MerkelNode
+): Promise<MerkelNode> {
+  if (!right) {
+    return left as BranchNode;
+  }
+  let branch = {
+    type: "branch",
     id: await hash([
       await hash(left.id),
       await hash(right.id),
-      await hash(intToBuffer(left.max))
+      await hash(intToBuffer(left.maxByteRange)),
     ]),
-    max: right.max
-  };
-}
+    byteRange: left.maxByteRange,
+    maxByteRange: right.maxByteRange,
+    leftChild: left,
+    rightChild: right,
+  } as BranchNode;
 
-async function hashLeaf(data: Uint8Array, note: number): Promise<HashNode> {
-  return {
-    id: await hash([await hash(data), await hash(intToBuffer(note))]),
-    max: note
-  };
+  return branch;
 }
 
 async function hash(data: Uint8Array | Uint8Array[]) {
@@ -82,7 +218,7 @@ async function hash(data: Uint8Array | Uint8Array[]) {
     data = Arweave.utils.concatBuffers(data);
   }
 
-  return await Arweave.crypto.hash(data);
+  return new Uint8Array(await Arweave.crypto.hash(data));
 }
 
 function intToBuffer(note: number): Uint8Array {
@@ -99,8 +235,8 @@ function bufferToInt(buffer: Uint8Array): number {
   return buffer.reduce((carry, current) => (carry + current) << 8, 0) >> 8;
 }
 
-const arrayCompare = (a: Uint8Array, b: Uint8Array) =>
-  a.every((value, index) => b[index] === value);
+export const arrayCompare = (a: Uint8Array | any[], b: Uint8Array | any[]) =>
+  a.every((value: any, index: any) => b[index] === value);
 
 export async function validatePath(
   id: Uint8Array,
@@ -130,7 +266,7 @@ export async function validatePath(
 
     const pathDataHash = await hash([
       await hash(pathData),
-      await hash(endOffsetBuffer)
+      await hash(endOffsetBuffer),
     ]);
 
     return arrayCompare(id, pathDataHash);
@@ -151,7 +287,7 @@ export async function validatePath(
   const pathHash = await hash([
     await hash(left),
     await hash(right),
-    await hash(offsetBuffer)
+    await hash(offsetBuffer),
   ]);
 
   if (arrayCompare(id, pathHash)) {
@@ -174,4 +310,40 @@ export async function validatePath(
   }
 
   return false;
+}
+
+/**
+ * Inspect an arweave chunk proof.
+ * Takes proof, parses, reads and displays the values for console logging.
+ * One proof section per line
+ * Format: left,right,offset => hash
+ */
+export async function debug(proof: Uint8Array, output = ""): Promise<string> {
+  if (proof.byteLength < 1) {
+    return output;
+  }
+
+  const left = proof.slice(0, HASH_SIZE);
+  const right = proof.slice(left.length, left.length + HASH_SIZE);
+  const offsetBuffer = proof.slice(
+    left.length + right.length,
+    left.length + right.length + NOTE_SIZE
+  );
+  const offset = bufferToInt(offsetBuffer);
+
+  const remainder = proof.slice(
+    left.length + right.length + offsetBuffer.length
+  );
+
+  const pathHash = await hash([
+    await hash(left),
+    await hash(right),
+    await hash(offsetBuffer),
+  ]);
+
+  const updatedOutput = `${output}\n${inspect(Buffer.from(left))},${inspect(
+    Buffer.from(right)
+  )},${offset} => ${inspect(pathHash)}`;
+
+  return debug(remainder, updatedOutput);
 }
--- test/chunks.ts
+++ test/chunks.ts
@@ -1,17 +1,103 @@
 import * as chai from "chai";
-import { b64UrlToBuffer } from "../src/common/lib/utils";
-import { validatePath } from "../src/common/lib/merkle";
+import { b64UrlToBuffer, bufferTob64Url } from "../src/common/lib/utils";
+import {
+  validatePath,
+  generateTree,
+  generateProofs,
+  computeRootHash,
+} from "../src/common/lib/merkle";
+import { readFileSync } from "fs";
+import { randomBytes } from "crypto";
+
 const expect = chai.expect;
 
-const root = b64UrlToBuffer("t-GCOnjPWxdox950JsrFMu3nzOE4RktXpMcIlkqSUTw");
-const path = b64UrlToBuffer(
-  "7EAC9FsACQRwe4oIzu7Mza9KjgWKT4toYxDYGjWrCdp0QgsrYS6AueMJ_rM6ZEGslGqjUekzD3WSe7B5_fwipgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAi3r3GojjGWpgoxl1BG6HssPgQRWqasSoNIOviZg8QI6z1utTnQ8OfE2tpn6vST0v3T72n7YW8U-Xy4k9ymX_iQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADAAAOfjIyBloFgUjZasOi_sydU1VkscXh4Rr9TfA-arh5V0AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAwAAA"
-);
+const rootB64Url = "t-GCOnjPWxdox950JsrFMu3nzOE4RktXpMcIlkqSUTw";
+const root = b64UrlToBuffer(rootB64Url);
+const pathB64Url =
+  "7EAC9FsACQRwe4oIzu7Mza9KjgWKT4toYxDYGjWrCdp0QgsrYS6AueMJ_rM6ZEGslGqjUekzD3WSe7B5_fwipgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAnH6dASdQCigcL43lp0QclqBaSncF4TspuvxoFbn2L18EXpQrP1wkbwdIjSSWQQRt_F31yNvxtc09KkPFtzMKAwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAIHiHU9QwOImFzjqSlfxkJJCtSbAox6TbbFhQvlEapSgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAAA";
+const path = b64UrlToBuffer(pathB64Url);
 
-const offset = 786431;
+const offset = 262143;
 const dataSize = 836907;
 
 describe("Chunks", function() {
+  const data = readFileSync("./test/rebar3");
+
+  it("should build a tree with a valid root", async function() {
+    const rootNode = await generateTree(data);
+
+    expect(bufferTob64Url(rootNode.id)).to.equal(rootB64Url);
+    // const sortedProofs = await Promise.all(
+    //   proofs.map(async (item) => {
+    //     return {
+    //       ...item,
+    //       proof: item.proof,
+    //       proofb64: bufferTob64Url(item.proof),
+    //       debug: await debug(item.proof),
+    //     };
+    //   })
+    // );
+    // console.log("Root hash", bufferTob64Url(rootHash));
+    // console.log(inspect(sortedProofs, false, null, true));
+    // const toTest = sortedProofs[0];
+  });
+
+  it("should build valid proofs from tree", async function() {
+    const rootNode = await generateTree(data);
+    const proofs = await generateProofs(rootNode);
+    expect(bufferTob64Url(proofs[0].proof)).to.equal(pathB64Url);
+  });
+
+  it("should validate own proofs and reject invalid verification parameters", async function() {
+    const rootNode = await generateTree(data);
+    const rootHash = await computeRootHash(data);
+    const proofs = await generateProofs(rootNode);
+
+    type Args = [Uint8Array, number, number, number, Uint8Array];
+
+    const testInput: Args = [
+      rootHash,
+      proofs[0].offset,
+      0,
+      data.byteLength,
+      proofs[0].proof,
+    ];
+
+    const didValidate = await validatePath.apply(validatePath, testInput);
+
+    expect(didValidate).to.equal(true);
+
+    const invalidInputA: Args = [
+      rootHash,
+      proofs[0].offset,
+      0,
+      data.byteLength,
+      randomBytes(256), // invalid proof
+    ];
+
+    const didValidateWithInvalidInputA = await validatePath.apply(
+      validatePath,
+      invalidInputA
+    );
+
+    expect(didValidateWithInvalidInputA).to.equal(false);
+
+    const invalidInputB: Args = [
+      randomBytes(32), // invalid root node
+      proofs[0].offset,
+      0,
+      data.byteLength,
+      proofs[0].proof,
+    ];
+
+    const didValidateWithInvalidInputB = await validatePath.apply(
+      validatePath,
+      invalidInputB
+    );
+
+    expect(didValidateWithInvalidInputB).to.equal(false);
+  });
+
   it("should validate a valid data path against a valid data root", async function() {
     expect(await validatePath(root, offset, 0, dataSize, path)).to.equal(true);
   });
@@ -31,22 +117,6 @@ describe("Chunks", function() {
     );
 
     expect(await validatePath(root, offset, 0, dataSize, invalidPath)).to.equal(
-      false
-    );
-  });
-
-  it("should reject invalid offset", async function() {
-    const invalidOffset = 999;
-
-    expect(await validatePath(root, invalidOffset, 0, dataSize, path)).to.equal(
-      false
-    );
-  });
-
-  it("should reject invalid data size", async function() {
-    const invalidDataSize = 999;
-
-    expect(await validatePath(root, offset, 0, invalidDataSize, path)).to.equal(
       false
     );
   });
