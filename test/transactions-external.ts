d3799f782d477f835bbcf69bb761ea535dbe6502
Luca Cervone <you@example.com>
2021-01-08 10:48:26 +0100

    adding tests for transactions created with external tools

---
+++ test/transactions-external.ts
@@ -0,0 +1,32 @@
+import * as chai from "chai";
+import * as crypto from "crypto";
+import Transaction from "../src/common/lib/transaction";
+import { arweaveInstance, arweaveInstanceDirectNode } from "./_arweave";
+
+const expect = chai.expect;
+
+const arweave = arweaveInstance();
+
+const digestRegex = /^[a-z0-9-_]{43}$/i;
+
+// This transaction was created by using AWS keys
+const externalTransaction = "DheCVCoV7HcZHu5qxQUAlJJRYdfeOkP4oVgFJok2pWg";
+
+describe("External Transactions", function() {
+  it("should verify transactions created without arweave-js", async function() {
+    this.timeout(10000);
+
+    const wallet = await arweave.wallets.generate();
+
+    // get the transaction created externally
+    const transaction = await arweave.transactions.get('DheCVCoV7HcZHu5qxQUAlJJRYdfeOkP4oVgFJok2pWg');
+
+    console.log(transaction);
+
+    const verified = await arweave.transactions.verify(transaction);
+
+    expect(verified).to.be.a("boolean");
+
+    expect(verified).to.be.true;
+  });
+});
