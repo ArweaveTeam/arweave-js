087d31509f0c0d1e0f52b72f1c168f79be142ec4
Marton Lederer <marton@lederer.hu>
2023-01-04 21:51:31 +0100

    feat: try polyfilling headers

--- src/node/index.ts
+++ src/node/index.ts
@@ -1,9 +1,10 @@
 import Arweave from "./common";
-import fetch from "node-fetch";
+import fetch, { Headers as NodeHeaders } from "node-fetch";
 import { ApiConfig } from "./lib/api";
 
 // @ts-expect-error
 global.fetch = fetch;
+global.Headers = NodeHeaders;
 
 Arweave.init = function (apiConfig: ApiConfig = {}): Arweave {
   return new Arweave(apiConfig);
