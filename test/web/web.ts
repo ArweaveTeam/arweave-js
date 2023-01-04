0ff41184b74a97aa428d36fac6db4d9e8d58f1b8
Marton Lederer <marton@lederer.hu>
2023-01-04 21:45:34 +0100

    feat: add back external implicit wallet testing

--- test/web/web.ts
+++ test/web/web.ts
@@ -192,7 +192,7 @@ describe("Transactions", function () {
     expect(verifiedWithModififedTags).to.be.false;
   });
 
-  /*it("should create and sign transactions using external implicit wallet", async function () {
+  it("should create and sign transactions using external implicit wallet", async function () {
     this.timeout(120000);
 
     const transaction = await arweave.createTransaction({ data: "test" });
@@ -200,7 +200,7 @@ describe("Transactions", function () {
 
     const verified = await arweave.transactions.verify(transaction);
     expect(verified).to.be.a("boolean").and.to.be.true;
-  });*/
+  });
 
   it("should get transaction info", async function () {
     this.timeout(5000);
